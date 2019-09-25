07760a5541eae76309866842cbf937050d0ebbc6
pshusang <38292263+pshusang@users.noreply.github.com>
2019-09-25 09:18:25 -0400

    Rename from patient to index

--- app/public/api/records/index.php
+++ app/public/api/records/index.php
@@ -5,18 +5,17 @@ $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
 $stmt = $db->prepare('SELECT * FROM Patient');
+//Take SQL code and prepare it into a string
 $stmt->execute();
+//Statement object run the query and it has the access to the return data
 $patients = $stmt->fetchAll();
-
-// patientGuid VARCHAR(64) PRIMARY KEY,
-// firstName VARCHAR(64),
-// lastName VARCHAR(64),
-// dob DATE DEFAULT NULL,
-// sexAtBirth CHAR(1) DEFAULT ''
+//Fetch the result of the query
 
 // Step 3: Convert to JSON
 $json = json_encode($patients, JSON_PRETTY_PRINT);
+//JSON_PRETTY_PRINT formats the code
 
 // Step 4: Output
 header('Content-Type: application/json');
+//Design for a connection through JSON
 echo $json;
