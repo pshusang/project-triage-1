7077989edbc2b7b5912910d183609fcd0ffac51c
pshusang <38292263+pshusang@users.noreply.github.com>
2019-09-25 09:40:23 -0400

    Create another api end-point

--- app/data/schema-patient.sql
+++ app/data/schema-patient.sql
@@ -14,11 +14,14 @@ INSERT INTO Patient (patientGuid, firstName, lastName, sexAtBirth, dob) VALUES
 ("SOME-UNIQUE-ABCDE1", "J", "Doe", "1950",  ""),
 ("SOME-DUMMY-DATA", "Pepper", "Potts", "1990-01-31",  "F");
 
-CREATE TABLE PatientVisit (
+CREATE TABLE PatientVisit
+(
     visitID INTEGER PRIMARY KEY AUTO_INCREMENT,
     patientGuid VARCHAR(64) UNIQUE,
-    visitDescription TEXT NOT NULL DEFAULT,
+    visitDescription TEXT NOT NULL,
     visitDateUtc DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    priority ENUM('Low','Medium','High') NOT NULL DEFAULT,
-    FOREIGN KEY patientGuid References Patient(patientGuid)
+    priority ENUM('Low','Medium','High') NOT NULL DEFAULT 'Low'
 );
+
+INSERT INTO PatientVisit (visitID, patientGuid, visitDescription) VALUES
+(1, 'SOME-REALLY-LONG-1234', 'Anxiety from D&S');
---
+++ app/public/api/waiting/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// Step 1: Get a database connection from our help class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$stmt = $db->prepare('SELECT * FROM PatientVisit');
+//Take SQL code and prepare it into a string
+$stmt->execute();
+//Statement object run the query and it has the access to the return data
+$patients = $stmt->fetchAll();
+//Fetch the result of the query
+
+// Step 3: Convert to JSON
+$json = json_encode($patients, JSON_PRETTY_PRINT);
+//JSON_PRETTY_PRINT formats the code
+
+// Step 4: Output
+header('Content-Type: application/json');
+//Design for a connection through JSON
+echo $json;
