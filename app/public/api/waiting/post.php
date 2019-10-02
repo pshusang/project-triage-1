c73546fd73ffc1a5eaffa00c977df9190f635a14
pshusang <38292263+pshusang@users.noreply.github.com>
2019-10-02 11:15:38 -0400

    Update TODO

--- app/public/api/waiting/post.php
+++ app/public/api/waiting/post.php
@@ -15,3 +15,11 @@ $stmt->execute([
   $_POST['visitDescription'],
   $_POSt['priority']
 ]);
+
+// TODO: Error Checking
+
+//Step 4: Output
+header('HTTP/1.1 303 See other');
+//300 redirect with a git
+header('Location: ../waiting/');
+//Here is where to go
